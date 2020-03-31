f0a0479ed44df3544139e656df70182e6205bb20
Daniel Wiese <dpwiese@gmail.com>
2020-03-31 19:59:24 -0400

    install react-native-ble-plx

---
+++ ios/ReactNativeCatalyst/File.swift
@@ -0,0 +1,10 @@
+//
+//  File.swift
+//  ReactNativeCatalyst
+//
+//  Created by Daniel Wiese on 3/29/20.
+//
+//  Emmpty file for react-native-ble-plx integration purposes.
+//  For further info head to: https://github.com/Polidea/react-native-ble-plx#configuration--installation
+//
+//  import Foundation
