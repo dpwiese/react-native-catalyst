80cd911321c750be4006d5f1cf316ba0f45532a0
Daniel Wiese <dpwiese@gmail.com>
2021-01-31 15:36:11 -0500

    fix tests

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,10 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  env: {
+    test: {
+      plugins: [
+        '@babel/plugin-proposal-class-properties',
+      ],
+    },
+  },
 };
