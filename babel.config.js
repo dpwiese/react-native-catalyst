43e27b1cf05dc257a247c489f515950913ee2d6e
Daniel Wiese <dpwiese@gmail.com>
2021-01-31 15:46:21 -0500

    dont need to update babel to fix tests
    only needed react-native-webview as dependency in project

--- babel.config.js
+++ babel.config.js
@@ -1,10 +1,3 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
-  env: {
-    test: {
-      plugins: [
-        '@babel/plugin-proposal-class-properties',
-      ],
-    },
-  },
 };
